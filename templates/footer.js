f9a4aeec526b3442dd4df4f22315a74b3b0e07dc
Glitch (glitch-in-bio) <none>
2023-10-14 19:06:29 +0000

    🐽👧 Checkpoint ./templates/footer.js:61950657/223 ./public/about.html:61950657/109

--- templates/footer.js
+++ templates/footer.js
@@ -5,6 +5,7 @@ export const renderFooter = html.node`<footer class="footer">
   <div class="tagline" style="color:darkred">
     Photography: <a href="https://www.instagram.com/nocta_photography/">@nocta_photography</a> <br />
     Design: <a href="https://www.instagram.com/alchemicalowl/">@alchemicalowl</a> <br />
+    Created and hosted with <a href="https://glitch.com/">glitch</a>
   </div>
 </footer>
 `;
