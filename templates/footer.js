b25dcdd5cefe93532eb55d91092fa9c19df94727
Glitch (glitch-in-bio) <none>
2025-03-06 11:42:34 +0000

    🌷🍚 Checkpoint ./templates/footer.js:61950657/436 ./public/about-cat.html:61950657/69012 ./public/about-en.html:61950657/713 ./public/about-fr.html:61950657/713 ./public/about.html:61950657/703

--- templates/footer.js
+++ templates/footer.js
@@ -3,11 +3,12 @@ import settings from "../settings.json";
 
 export const renderFooter = html.node`<footer class="footer">
   <div class="tagline" style="color:darkred">
-    Photography: <a href="https://www.instagram.com/nocta_photography/">@nocta_photography</a>,
-      Paula Gutiérrez,
-      <a href="https://www.instagram.com/danifarrus/"
-      >@danifarrus</a
-      > <br />
+    Photography: <a href="https://www.instagram.com/nocta_photography/">@nocta_photography</a>,<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="https://www.instagram.com/dani_morell/"
+      >@dani_morell</a
+      >, Paula Gutiérrez
+       <br />
     Design: <a href="https://www.instagram.com/alchemicalowl/">@alchemicalowl</a> <br />
     Created with <a href="https://glitch.com/">Glitch</a> and served by <a href="https://www.fastly.com/">Fastly</a> 
   </div>
