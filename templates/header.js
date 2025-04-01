786022086103cb392ae7669e29bda01d101d8b46
Glitch (glitch-in-bio) <none>
2025-04-01 17:52:17 +0000

    🎙😄 Checkpoint ./settings.json:61950657/706 ./templates/header.js:61950657/165 ./public/styles/style.css:61950657/6

--- templates/header.js
+++ templates/header.js
@@ -5,5 +5,5 @@ export const renderHeader = html.node`<div>
   <div class='avatar-container'>
     <img src='${settings.avatarImage}' class='avatar' alt='${settings.name}' />
   </div>
-  <h1> <a href="https://www.instagram.com/p/C5THW6Vijgu/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">${settings.name}</a></h1>
+  <h1> <a href="https://open.spotify.com/album/723yOSHWeJRcBQZS0JJzLk?autoplay=true">${settings.name}</a></h1>
 </div>`;
